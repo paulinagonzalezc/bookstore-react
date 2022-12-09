8de20b2398a9e29205faf0f7e485a367bb03c4c3
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-12-08 22:39:30 -0500

    Add all required content for each component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Book from './Book';
+import Inputs from './Inputs';
 
-function Books() {
-  return (
-    <div>Books</div>
-  )
-}
+const listBooks = [
+  { title: 'The Hunger Games', author: 'Suzanne Collins', category: 'Action' },
+  { title: 'Dune', author: 'Frank Herbert', category: 'Science Fiction' },
+  { title: 'Capital in the Twenty-First Century', author: 'Suzanne Collins', category: 'Economy' },
+];
 
-export default Books
+const Books = () => (
+  <div className="books-container">
+    <ul className="books">
+      <Book
+        title={listBooks[0].title}
+        author={listBooks[0].author}
+        category={listBooks[0].category}
+      />
+      <Book
+        title={listBooks[1].title}
+        author={listBooks[1].author}
+        category={listBooks[1].category}
+      />
+      <Book
+        title={listBooks[2].title}
+        author={listBooks[2].author}
+        category={listBooks[2].category}
+      />
+    </ul>
+    <div className="horizontal-divider" />
+    <Inputs />
+  </div>
+);
+
+export default Books;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Nav() {
-  return (
-    <nav className="nav-container">
-      <h1 className="header">Bookstore CMS</h1>
-      <div className="nav-links">
-        <li><Link to="/">Books</Link></li>
-        <li><Link to="/categories">Categories</Link></li>
-      </div>
-    </nav>
-  );
-}
+const Nav = () => (
+  <nav className="nav-bar">
+    <div className="nav-brand">Bookstore CMS</div>
+    <ul className="nav-links">
+      <li><Link to="/">BOOKS</Link></li>
+      <li><Link to="/categories">CATEGORIES</Link></li>
+    </ul>
+    <button className="icon-button" type="button">
+      <span className="material-icons">person</span>
+    </button>
+  </nav>
+);
 
 export default Nav;
