c68d16ef003f6aaa2f29a2f2ed7e3247027ee36f
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-12-08 23:54:49 -0500

    Change multiple Books into map statement

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,29 +3,37 @@ import Book from './Book';
 import Inputs from './Inputs';
 
 const listBooks = [
-  { title: 'The Hunger Games', author: 'Suzanne Collins', category: 'Action' },
-  { title: 'Dune', author: 'Frank Herbert', category: 'Science Fiction' },
-  { title: 'Capital in the Twenty-First Century', author: 'Suzanne Collins', category: 'Economy' },
+  {
+    id: 1,
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    category: 'Action',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+    category: 'Science Fiction',
+  },
+  {
+    id: 3,
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    category: 'Economy',
+  },
 ];
 
 const Books = () => (
   <div className="books-container">
     <ul className="books">
-      <Book
-        title={listBooks[0].title}
-        author={listBooks[0].author}
-        category={listBooks[0].category}
-      />
-      <Book
-        title={listBooks[1].title}
-        author={listBooks[1].author}
-        category={listBooks[1].category}
-      />
-      <Book
-        title={listBooks[2].title}
-        author={listBooks[2].author}
-        category={listBooks[2].category}
-      />
+      {listBooks.map((book) => (
+        <Book
+          title={book.title}
+          author={book.author}
+          category={book.category}
+          key={book.id}
+        />
+      ))}
     </ul>
     <div className="horizontal-divider" />
     <Inputs />
